c56fa189dbd6e88a06ec97ccc84d424f1dca4d12
Novvator <stamatis.sellas@yahoo.com>
2023-04-16 14:07:20 +0300

    added find watchlist in generate_data

---
+++ generate_data.py
@@ -0,0 +1,66 @@
+import re
+from bs4 import BeautifulSoup
+import requests
+import pprint
+
+
+def find_number_of_films_by_genre(username=None):
+    numbers = {}
+    genres = ['action','adventure', 'animation', 'comedy', 'crime', 'documentary', 'drama', 'family',
+        'fantasy', 'history', 'horror', 'music', 'mystery', 'romance', 'science-fiction', 'thriller', 'tv-movie', 'war', 'western']
+    username = 'vaflacko'
+
+
+    for genre in genres:
+        # Use beautiful soup to go to page
+        url = 'https://letterboxd.com/'+ username + '/films/genre/' + genre
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content,features="html.parser")
+        element = soup.find("p", {"class" : "ui-block-heading"}).text
+
+        # Use regular expression to extract number of watched movies
+        match = re.search(r'(?<=watched\s)\d+', element)
+        if match:
+            number = int(match.group())
+            print(number)
+            numbers[genre] = number
+        else:
+            print("No match found")
+    pprint.pprint(numbers)
+    return numbers
+
+def generateLink(URL, x):
+    return str(URL) + str(x)
+
+def find_watchlist(username=None):
+    username = 'vaflacko'
+    x = 1
+    watchlist = []
+    movieresults = []
+    url = 'https://letterboxd.com/' + username + '/watchlist/page/'
+
+    while(True):
+    
+    # generate link and parse with soup
+        link = generateLink(url, x)
+        page = requests.get(link)
+        soup = BeautifulSoup(page.content,features="html.parser")
+
+        # find movies element
+        currmovieresults = soup.find_all("img", {"class" : "image"})
+        movieresults += currmovieresults
+
+        # if there is next page button continue loop
+        next_button = soup.find("a", {"class":"next"})
+        if not next_button:
+            break
+
+        x += 1
+    
+    for element in movieresults:
+        watchlist.append(element['alt'])
+    print(watchlist)
+    return watchlist
+
+if __name__=="__main__":
+    find_watchlist()
